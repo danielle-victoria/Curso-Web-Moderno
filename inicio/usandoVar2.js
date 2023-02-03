8ea17225522e762999b8be40ed7b77b6e01ee8bd
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-02-03 20:56:19 -0300

    Usando var - Parte 2

---
+++ inicio/usandoVar2.js
@@ -0,0 +1,8 @@
+var numero = 1
+{
+    var numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero) //sobrescrita de numero 
+
+//Dica -> Em JavaScript, fuja do escopo global mais no lado do front-end
