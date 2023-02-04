fc7cd3b94843ebebf5d85fae0c5cfd9745f83038
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-02-03 22:20:35 -0300

    Função Vs Objeto

---
+++ inicio/objeto2.js
@@ -0,0 +1,14 @@
+console.log(typeof Object)
+console.log(typeof new Object) //instanciando uma função
+
+const Cliente = function(){}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {}//ES 2015 {ES6}
+console.log(typeof Produto)
+console.log(typeof new Produto)
+
+//Função exerce o papel de uma classe, pois ela é
+//um molde da sua estrutura(definir as funções e as
+//variáveis) para pode fazer várias instâncias.
