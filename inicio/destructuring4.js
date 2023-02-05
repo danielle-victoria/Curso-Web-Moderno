bcbb18582825dba3c71f28857082c5cec6f7f2f9
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-02-05 16:03:19 -0300

    Operador Destructuring - Function + Array

---
+++ inicio/destructuring4.js
@@ -0,0 +1,13 @@
+// Operador Destructuring - Function + Array
+
+function rand([min = 0, max = 1000]){
+    if(min > max) [min, max] = [max, min]
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+console.log(rand([50, 40]))
+console.log(rand([992]))
+console.log(rand([, 10]))
+console.log(rand([]))
+console.log(rand()) //Vai tentar "desestruturar" algo que está undefined //Erro
