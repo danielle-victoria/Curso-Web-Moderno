ecd64071005804d9b35495d2ef88487dcfae2d42
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-02-05 17:25:35 -0300

    Contexto de Execução - Node

---
+++ inicio/ContextoDeExecucao.js
@@ -0,0 +1,20 @@
+//Contexto de Execução - Node
+let a = 3
+
+global.b = 123
+this.c = 456
+this.d = false
+this.e = 'teste'
+
+console.log(a)
+console.log(global.b)
+console.log(this.c)
+console.log(module.exports.c)
+console.log(module.exports === this)
+console.log(module.exports)
+
+// Criando uma variável maluca: sem var e let!
+abc = 3 // Não fazer isso
+console.log(global.abc)
+
+// module.exports = { e: 456, f: false, g: 'teste'}
