7457b57f01faf1e8ea58dda72e2d51b1177cdd65
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-12-29 17:25:05 -0300

    Aula 20 - Funções: Classe vs Factory

---
+++ funcao/classeVsFactory.js
@@ -0,0 +1,25 @@
+// Aula 20 - Funções: Classe vs Factory 
+
+
+class Pessoa {
+    constructor(nome){
+        this.nome = nome
+    }
+
+    falar(){
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+
+const criarPessoa = nome => {
+    return{
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('João')
+p2.falar()
