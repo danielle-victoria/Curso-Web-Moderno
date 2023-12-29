1eebf5ca16539e2212075d8efbf86927112c40d4
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-12-29 10:50:54 -0300

    Aula 10 - Funções: Anônimas

---
+++ funcao/funcoesAnonimas.js
@@ -0,0 +1,30 @@
+// Aula 10 - Funções: Anônimas 
+
+// Função Anônima - Função sem nome
+
+const soma = function (x, y){
+    return x + y
+} 
+
+const imprimirResultado = function(a, b , operacao = soma){
+    console.log(operacao(a, b))
+}
+
+imprimirResultado(3,4)
+imprimirResultado(3,4, soma)
+imprimirResultado(3,4, function(x,y){
+    return x - y
+})
+
+imprimirResultado(3,4, (x,y) => x * y)
+
+
+// Criação de uma função dentro de um objeto
+
+const pessoa = {
+    falar: function (){
+        console.log('Opa')
+    }
+}
+
+pessoa.falar()
