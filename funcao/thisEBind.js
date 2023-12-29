c0ecb76b6e23381e7373dd428bbc863c28304232
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-12-28 23:39:43 -0300

    Aula 5 - Funções: This e Bind

---
+++ funcao/thisEBind.js
@@ -0,0 +1,17 @@
+// Aula 5 - Funções: This e Bind
+
+
+const pessoa = {
+    saudacao: 'Bom dia',
+    falar(){
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+
+const falar = pessoa.falar
+falar() // conflito entre paradigmas: funcional e OO
+
+const falarDePessoa = pessoa.falar.bind(pessoa) // bind resolve esse conflito
+falarDePessoa()
