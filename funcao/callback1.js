7091ccfe024fc064e182151fc77d426c2a0130b5
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-12-29 10:58:29 -0300

    Aula 11 - Funções: Callback - 2

---
+++ funcao/callback1.js
@@ -0,0 +1,15 @@
+// Aula 11 - Funções: Callback - 1
+
+
+const fabricantes = ['Mercedes', 'Audi', 'BMW']
+
+function imprimir(nome, indice){
+    console.log(`${indice+1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(function(fabricante){
+    console.log(fabricante)
+})
+
+fabricantes.forEach(fabricante => console.log(fabricante))
