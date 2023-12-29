9c024695cba409424b763b16cf0de0866ddf0d07
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-12-29 17:06:43 -0300

    Aula 17 - Funções: Closures

---
+++ funcao/closure.js
@@ -0,0 +1,17 @@
+// Aula 17 - Funções: Closures
+
+// Definição: Closure é o escopo criado quando uma função é declarada
+// Esse escopo permite a função acessar e manipular variáveis externas à função
+
+const x = 'Global'
+
+function fora(){
+    const x = 'Local'
+    function dentro (){
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
