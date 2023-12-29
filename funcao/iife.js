16e6965d0bdd8a1452b94303f9ebf221a25357d3
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-12-29 17:34:50 -0300

    Aula 22 - Funções: IIFE

---
+++ funcao/iife.js
@@ -0,0 +1,11 @@
+// Aula 22 - Funções: IIFE
+
+// IIFE -> Immediately Invoked Function Expression
+
+(function(){
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
+
+
+// Padrão para fugir do escopo global
