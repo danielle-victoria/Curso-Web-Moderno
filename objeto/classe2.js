cd9874b4ddeef4b8d331e7cc478abdc9f72f04ce
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-12-31 19:46:42 -0300

    Aula 17 - Objeto: Classe - 2

---
+++ objeto/classe2.js
@@ -0,0 +1,23 @@
+// Aula 17 - Objeto: Classe - 2
+
+class Avo {
+    constructor(sobrenome){
+        this.sobrenome = sobrenome
+    }
+}
+
+class Pai extends Avo{
+    constructor(sobrenome, profissao = 'Professor'){
+        super(sobrenome)
+        this.profissao = profissao
+    }
+}
+
+class Filho extends Pai{
+    constructor(){
+        super('Silva')
+    }
+}
+
+const filho = new Filho
+console.log(filho)
