f933cc3a0dca3d81c81a20d85b27df23c3b32bbf
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-12-31 11:27:32 -0300

    Aula 1 - Objeto: Introdução

---
+++ objeto/oo.js
@@ -0,0 +1,26 @@
+// Aula 1 - Objeto: Introdução
+
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural -> Baseado no procedimento/função
+//Função que manipula dados
+procedimento(valor1, valor2, valor3)
+
+// OO -> Têm-se dados, dentro dos dados, funções.
+
+objeto = {
+    valor1, //atributo
+    valor2,
+    valor3,
+    processamento(){ //comportamento
+        //...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. Polimorfismo
