217a0fad771a975ab01aed82f75b0e3c3e0a56a6
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-01 11:37:01 -0300

    Aula 3 - Array: Simulando Array Com Objeto

---
+++ array/simulandoArray.js
@@ -0,0 +1,13 @@
+// Aula 3 - Array: Simulando Array Com Objeto 
+
+const quaseArray = {0: 'Rafael', 1: 'Ana', 2: 'Bia'}
+console.log(quaseArray)
+Object.defineProperty(quaseArray, 'toString', {
+    value: function() { return Object.values(this)},
+
+})
+
+console.log(quaseArray[0])
+
+const meuArray = ['Rafael', 'Ana', 'Bia']
+console.log(quaseArray.toString(), meuArray)
