219704f7f85a780484289a3957aa6278e37abafe
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-01 17:00:58 -0300

    Aula 7 - Array: Map - 2

---
+++ array/map2.js
@@ -0,0 +1,18 @@
+// Aula 7 - Array: Map - 2
+
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+
+// retorna um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
+
+
