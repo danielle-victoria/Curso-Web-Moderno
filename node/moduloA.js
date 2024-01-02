1f85de8d1a4eee031e4c5b087acccc76dbff443b
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-02 17:29:45 -0300

    Aula 2 - Node: Sistema de Módulos - 1, 2 e 3

---
+++ node/moduloA.js
@@ -0,0 +1,6 @@
+// Aula 2 - Node: Sistema de Módulos - 1
+
+// Formas de exportar no Node
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem Vindo ao node!'
+module.exports.ateLogo = 'Até próximo exemplo'
