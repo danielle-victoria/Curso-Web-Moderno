6c0f2f7428e6972ccdc310969447a1b628ea90c4
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-03 10:34:23 -0300

    Aula 6  - Node: Arquivo package.json

---
+++ node/funcionarios/funcionarios.js
@@ -0,0 +1,10 @@
+// Aula 6  - Node: Arquivo package.json 
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+axios.get(url).then(response =>{
+    const funcionarios = response.data
+    console.log(funcionarios)
+    
+})
