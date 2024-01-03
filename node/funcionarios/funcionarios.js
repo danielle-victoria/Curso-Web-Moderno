00b5e81322c58caefe2bcdfeba78987492937dd5
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-03 10:44:25 -0300

    Aula 6 e 7 - Node: Arquivo package.json  e Resolvendo o Desafio

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,10 +1,37 @@
-// Aula 6  - Node: Arquivo package.json 
+// Aula 6 e 7 - Node: Arquivo package.json  e Resolvendo o Desafio
 
-const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+// Aula 6
+/*const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
 axios.get(url).then(response =>{
     const funcionarios = response.data
     console.log(funcionarios)
+    
+})*/
+
+
+// Aula 7 
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+axios.get(url).then(response =>{
+    const funcionarios = response.data
+    //console.log(funcionarios)
+
+    // Mulher chinesa com menor salario?
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+
+    console.log(func)
     
 })
