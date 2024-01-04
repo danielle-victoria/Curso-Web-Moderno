fce3a072531e921bc12fd2e995aa72affe25a322
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-04 17:23:34 -0300

    Aula 4 - ESNext: Operadores Rest/Spread

---
+++ ESNext/operadorSpreadRest.js
@@ -0,0 +1,16 @@
+// Aula 4 - ESNext: Operadores Rest/Spread
+
+// Operador ... rest(juntar) spread(espalhar)
+// Usar rest com parâmetro de função
+
+// Usar spread com objeto
+const funcionario = { nome: 'Maria', salario: 12348.99}
+const clone = { ativo: true, ...funcionario}
+
+console.log(clone)
+
+// Usar spread com array
+const grupoA = ['João', 'Pedro', 'Gloria']
+const grupoFinal = ['Maria',  ...grupoA, 'Rafaela']
+
+console.log(grupoFinal)
