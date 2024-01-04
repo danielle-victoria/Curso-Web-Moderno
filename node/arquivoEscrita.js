e86c92e2ff2495fa81b31d0bfcfcea105e462dda
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-03 22:33:29 -0300

    Aula 15 - Node: Escrevendo Arquivos

---
+++ node/arquivoEscrita.js
@@ -0,0 +1,13 @@
+// Aula 15 - Node: Escrevendo Arquivos
+
+const fs = require('fs')
+
+const produto = {
+    nome: 'Celular',
+    preco: 1249.99,
+    desconto: 0.15
+}
+
+fs.writeFileSync(__dirname + '/arquivoGerado.json', JSON.stringify(produto), err => {
+    console.log(err || 'Arquivo salvo!')
+})
