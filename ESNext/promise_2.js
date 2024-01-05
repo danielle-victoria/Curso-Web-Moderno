5d74d5c85bdafd73b272fe0c79d3e0fef06acadc
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-04 23:11:12 -0300

    Aula 15 - ESNext: Promise - 2

---
+++ ESNext/promise_2.js
@@ -0,0 +1,29 @@
+// Aula 15 - ESNext: Promise - 2
+
+/*setTimeout(function(){
+    console.log('Executando callback...')
+
+    setTimeout(function(){
+        console.log('Executando callback...')
+
+
+        setTimeout(function(){
+            console.log('Executando callback...')
+        },2000)
+    }, 2000)
+}, 2000)*/
+
+
+
+function esperarPor(tempo = 2000){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            console.log('Executando promise')
+            resolve()
+        }, tempo)
+    })
+}
+
+esperarPor()
+    .then(() => esperarPor())
+    .then(esperarPor)
