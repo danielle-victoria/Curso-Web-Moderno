e9d4d86f05e6bdecdedc677071c826c119b88078
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-09 22:35:41 -0300

    Aula 13: HTML - Formulário 1

---
+++ Exercicios-WEB/html/exercicios/form/server.js
@@ -0,0 +1,17 @@
+// Aula 13: HTML - Formulário 1 
+
+//Backend
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabéns</h1>')
+})
+
+
+app.listen(3003)
