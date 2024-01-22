eb88696d3e64e4e47e9cfbdd9af4d37d569dccb8
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-22 17:32:06 -0300

    Aula 3: Ajax - XML HTTP Request - 1

--- Exercicios-WEB/ajax/server.js
+++ Exercicios-WEB/ajax/server.js
@@ -9,5 +9,5 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true})) // responsável por transformar os dados
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
+//app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
