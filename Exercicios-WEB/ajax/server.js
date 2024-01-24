fc07301521e32b4b8e719e27640f13371287b1eb
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2024-01-24 14:59:01 -0300

    Aula 4: Ajax - XML HTTP Request - 2

--- Exercicios-WEB/ajax/server.js
+++ Exercicios-WEB/ajax/server.js
@@ -1,4 +1,6 @@
 // Aula 2: Ajax - Implementando server.js
+// Aula 4: Ajax - XML HTTP Request -- 2 
+
 
 
 const bodyParser = require('body-parser')
@@ -10,4 +12,28 @@ app.use(bodyParser.urlencoded({extended: true})) // responsável por transformar
 app.use(bodyParser.json())
 
 //app.get('/teste', (req, res) => res.send('Ok'))
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) { // define a pasta do arquivo
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`) // define o nome do arquivo
+    }
+})
+
+const upload = multer({ storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req,res, err => {
+        if(err) {
+            return res.send('Ocorreu um erro.')
+        }
+
+        res.send('Concluído com sucesso.')
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
