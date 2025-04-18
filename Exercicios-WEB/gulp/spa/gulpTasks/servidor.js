642b12cf07b0678e454b8c7cc2be23a8139166f8
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-04-18 15:16:49 -0300

    Projeto SPA: Criando a Estrutura do Build

---
+++ Exercicios-WEB/gulp/spa/gulpTasks/servidor.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+
+
+function monitoraArquivos(cb){
+    return cb()
+}
+
+function servidor(cb){
+    return cb()
+}
+
+module.exports = {
+    monitoraArquivos,
+    servidor
+}
