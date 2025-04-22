21f735487a062531f2edbd0340013d428d082c72
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-04-22 20:29:19 -0300

    Aula 7 - Desenvolvimento & Produção

--- Exercicios-WEB/webpack/webpack.config.js
+++ Exercicios-WEB/webpack/webpack.config.js
@@ -1,16 +1,33 @@
 /* Arquivo interpretado pelo node*/
 
+const modoDev = process.env.NODE_ENV !== 'production' // Aula 7 - Desenvolvimento & Produção
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+
 
 module.exports = {
-    mode: "development",
+    //mode: "development",
+    mode: modoDev ? 'development' : 'production',//Aula 7 - Desenvolvimento & Produção
     entry: "./src/principal.js", // ponto de entrada
     //entry: path.resolve(__dirname, './src/principal.js')
     output: {    /* Aula 3 - Configurando a Pasta de Saída */
         filename: 'principal.js',
         path: __dirname + '/public'  //Pasta de destino
     },
+    optimization: {
+        minimizer: [
+            new TerserWebpackPlugin({
+                //cache: true,
+                parallel: true
+            }),
+            new CssMinimizerWebpackPlugin({
+
+            })
+        ]
+    },
+
     plugins: [  // Aula 5 - Externalizando Arquivo .CSS
         new MiniCssExtractPlugin({
             filename: "estilo.css"
