08dca21bb74521ea703ab5acc98d8662005fcdd5
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-04-22 15:02:20 -0300

    Aula 3 - Configurando a Pasta de Saída

--- Exercicios-WEB/webpack/webpack.config.js
+++ Exercicios-WEB/webpack/webpack.config.js
@@ -4,7 +4,11 @@ const webpack = require('webpack')
 
 module.exports = {
     mode: "development",
-    entry: "./src/principal.js" // ponto de entrada
+    entry: "./src/principal.js", // ponto de entrada
     //entry: path.resolve(__dirname, './src/principal.js')
+    output: {    /* Aula 3 - Configurando a Pasta de Saída */
+        filename: 'principal.js',
+        path: __dirname + '/public'  //Pasta de destino
+    }
 }
 
