c9c4a1563b9c8fee469b9f8ea755445a06647cb8
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-05-31 16:02:55 -0300

    Aula 39 - React: Componente Com Parâmetro

--- Exercicios-WEB/react/exercicios/src/index.js
+++ Exercicios-WEB/react/exercicios/src/index.js
@@ -2,12 +2,18 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Primeiro from './componentes/Primeiro'
+//import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
 
 //ReactDOM.render(<h1>React</h1>, document.getElementById('root'))
 /*const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<h1>Projeto React</h1>)*/
 
 // Aula 38 - React: Primeiro Componente
+//OBS.: Não pode ter componente com a letra minúscula (import e nome do arquivo -> Maiusculos -> Manter o padrão)
+//const root = ReactDOM.createRoot(document.getElementById("root"))
+//root.render(<Primeiro/>)
+
+//Aula 39 - React: Componente Com Parâmetro
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Primeiro/>)
+root.render(<BomDia nome="Guilherme" idade={10} />)
