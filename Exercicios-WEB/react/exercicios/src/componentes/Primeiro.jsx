2af094626f3b532d846cd034d2c7a161a7e9155b
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-05-31 15:05:55 -0300

    Aula 38 - React: Primeiro Componente

---
+++ Exercicios-WEB/react/exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,9 @@
+// Aula 38 - React: Primeiro Componente
+
+import React from "react"
+
+/*export default function(){
+    return "Teste"
+}*/
+
+export default () => <h1>Primeiro</h1>
--- Exercicios-WEB/react/exercicios/src/index.js
+++ Exercicios-WEB/react/exercicios/src/index.js
@@ -2,7 +2,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Primeiro from './componentes/Primeiro'
 
 //ReactDOM.render(<h1>React</h1>, document.getElementById('root'))
+/*const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<h1>Projeto React</h1>)*/
+
+// Aula 38 - React: Primeiro Componente
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<h1>Projeto React</h1>)
+root.render(<Primeiro/>)
