4177109dbf3539831d6fb78395b242def3c8303a
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-03 20:31:53 -0300

    Aula 41 - React: Componente de Classe #01

---
+++ Exercicios-WEB/react/exercicios/src/componentes/Saudacao.jsx
@@ -0,0 +1,40 @@
+//Aula 41 - React: Componente de Classe #01
+
+import React, {Component} from "react"
+
+export default class Saudacao extends Component {
+
+    state = {
+        //  Fixo
+       /* tipo: "Fala",
+        nome: "Pedro"*/
+
+        tipo: this.props.tipo,
+        nome: this.props.nome
+
+    }
+
+    setTipo(e){
+        /*let i = setInterval(() => {
+            this.setState({tipo: ++i})
+        }, 1000)*/
+        this.setState({tipo: e.target.value})
+
+    }
+
+    SetNome(e){
+        this.setState({nome: e.target.value})
+
+    }
+    render(){
+        const {tipo, nome} = this.state
+        return (
+            <div>
+                <h1>{tipo} {nome}!</h1>
+                <hr/>
+                <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)} />
+                <input type="text" placeholder="Nome..." value={nome} onChange={e => this.SetNome(e)} />
+            </div>
+        )
+    }
+}
