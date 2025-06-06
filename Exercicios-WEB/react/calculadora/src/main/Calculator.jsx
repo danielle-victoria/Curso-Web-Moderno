acb79204deaf8c24be79c2432350fae11802c6a6
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-06 17:26:01 -0300

    Aula 2 - React - Projeto Calculadora: Componente Calculator

---
+++ Exercicios-WEB/react/calculadora/src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+// Aula 2 - React - Projeto Calculadora: Componente Calculator
+
+import React, {Component} from "react";
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return(
+            <div className="calculator">
+            
+            </div>
+        )
+    }
+}
