42330562a8dd0c59cf356633c84ca0390a5340b6
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-06 17:40:18 -0300

    Aula 3 - React - Projeto Calculadora: Componente Button #01

---
+++ Exercicios-WEB/react/calculadora/src/components/Button.jsx
@@ -0,0 +1,9 @@
+/* Aula 3 - React - Projeto Calculadora: Componente Button #01 */
+
+
+import React from "react"
+import './Button.css'
+
+
+export default props => 
+    <button className="button">{props.label}</button>
