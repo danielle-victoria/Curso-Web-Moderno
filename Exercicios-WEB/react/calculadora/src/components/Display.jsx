993f548c37e8bba823e6dd130a6511ada66d0994
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-07 14:35:34 -0300

    Aula 4 - React - Projeto Calculadora: Componente Display

---
+++ Exercicios-WEB/react/calculadora/src/components/Display.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import './Display.css'
+
+{/* Aula 4 - React - Projeto Calculadora: Componente Display */}
+
+export default props =>
+    <div className="display">{props.value}</div>
