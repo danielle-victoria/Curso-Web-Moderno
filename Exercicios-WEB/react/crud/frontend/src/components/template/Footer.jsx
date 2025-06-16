537c924c40ee822e239509566117db0c97f3a459
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-16 20:59:29 -0300

    Aula 7 - Projeto Cadastro de Usuário: Componente Rodapé

--- Exercicios-WEB/react/crud/frontend/src/components/template/Footer.jsx
+++ Exercicios-WEB/react/crud/frontend/src/components/template/Footer.jsx
@@ -5,5 +5,8 @@
  */}
  export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+                <strong> Code<span className="text-danger">3</span>r</strong>
+        </span>
     </footer>
