3a931a56f8215566bbec3c91119da98365638b8a
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-17 20:58:13 -0300

    Aula 9 - Projeto Cadastro de Usuário: Componente Conteúdo

---
+++ Exercicios-WEB/react/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Main from '../template/Main'
+
+{/* Aula 9 - Projeto Cadastro de Usuário: Componente Conteúdo */}
+
+
+export default props =>
+    <Main icon="home" title="Início"
+      subtitle="Segundo Projeto do capítulo de React.">
+      <div className='display-4'>Bem-Vindo!</div>
+      <hr/>
+      <p className="mb-0">Sistema para exemplificar a construção
+          de um cadastro desenvolvido em React!</p>
+      </Main>
--- Exercicios-WEB/react/crud/frontend/src/components/template/Main.jsx
+++ Exercicios-WEB/react/crud/frontend/src/components/template/Main.jsx
@@ -4,11 +4,16 @@ import Header from './Header'
 
 {/* Aula 3 - Projeto Cadastro de Usuário: Criando Componentes (Template)
  */}
+
+ {/* Aula 9 - Projeto Cadastro de Usuário: Componente Conteúdo */}
  
 export default props =>
     <React.Fragment>
         <Header {...props}/>
-        <main className="content">
+        <main className="content container-fluid">
+            <div className="p-3 mt-3">
+                {props.children}
+            </div>
             Conteúdo
         </main>
     </React.Fragment>
