7e1554291a3480f02047eca240107f6f2c2a8497
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-17 20:40:49 -0300

    Aula 8 - Projeto Cadastro de Usuário: Componente Navegação

--- Exercicios-WEB/react/crud/frontend/src/components/template/Nav.jsx
+++ Exercicios-WEB/react/crud/frontend/src/components/template/Nav.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 
 {/* Aula 3 - Projeto Cadastro de Usuário: Criando Componentes (Template)
  */}
+
+ {/* Aula 8 - Projeto Cadastro de Usuário: Componente Navegação */}
  
 export default props =>
     <aside className="menu-area">
+        <nav className='menu'>
+            {/* Refatorar */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
         Navegação
     </aside>
