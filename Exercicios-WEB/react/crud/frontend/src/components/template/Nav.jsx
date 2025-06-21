7ea0322a6d311822d09982984bc1f9e4b02c0b6f
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-21 17:52:59 -0300

    Aula 11 - Projeto Cadastro de Usuário: Implementando Rotas (React Router)

--- Exercicios-WEB/react/crud/frontend/src/components/template/Nav.jsx
+++ Exercicios-WEB/react/crud/frontend/src/components/template/Nav.jsx
@@ -1,5 +1,6 @@
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 {/* Aula 3 - Projeto Cadastro de Usuário: Criando Componentes (Template)
  */}
@@ -10,12 +11,12 @@ export default props =>
     <aside className="menu-area">
         <nav className='menu'>
             {/* Refatorar */}
-            <a href="#/">
+            <Link to ="/">
                 <i className="fa fa-home"></i> Início
-            </a>
-            <a href="#/users">
+            </Link>
+            <Link to ="/users">
                 <i className="fa fa-users"></i> Usuários
-            </a>
+            </Link>
         </nav>
         Navegação
     </aside>
