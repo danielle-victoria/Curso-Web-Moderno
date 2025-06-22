fa925fa05b1c7e9f8422507ebdd0d7e4dcdc669c
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-21 22:53:11 -0300

    Aula 12 - Projeto Cadastro de Usuário: Componente Cadastro Usuário #02

--- Exercicios-WEB/react/crud/frontend/src/components/user/UserCrud.jsx
+++ Exercicios-WEB/react/crud/frontend/src/components/user/UserCrud.jsx
@@ -1,15 +1,46 @@
-import React, {Component} from "react"
+import React, {Component, use} from "react"
+import axios from 'axios'
 import Main from "../template/Main"
 
 {/* Aula 10 - Projeto Cadastro de Usuário: Componente Cadastro Usuário #01 */}
-
+{/* Aula 12 - Projeto Cadastro de Usuário: Componente Cadastro Usuário #02 */}
 const headerProps = {
     icon: 'users',
     title: 'Usuários',
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!' 
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: ''},
+    list: []
+}
+
 export default class UserCrud extends Component {
+
+    state = { ...initialState }
+
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState.user, list})
+            })
+    }
+
+    getUpdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps}>
