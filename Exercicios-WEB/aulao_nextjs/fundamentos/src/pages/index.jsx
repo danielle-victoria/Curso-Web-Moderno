0eda9e9909ec644465cd75698cb6a52f16820709
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-28 16:28:19 -0300

    Aula 5 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Entendendo o JSX

--- Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/index.jsx
+++ Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -1,3 +1,14 @@
+{/* Aula 4 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Primeiro Componente & Rotas */}
+{/* Aula 5 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Entendendo o JSX */}
+
+
 export default function Inicio(){
-    return "Inicio"
+    //"<h1>Título</h1>" - Assim não é interpretado como html
+    return(
+        <div>
+            <h1>Fundamentos de Next.Js & React</h1>
+            <h2>Vamos estudar esse framework</h2>
+        </div>
+        
+    ) 
 }
