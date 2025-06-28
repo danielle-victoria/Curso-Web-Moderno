dfe3b5f4d09b01989c7f510ed4eef4993924216d
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-28 17:37:16 -0300

    Aula 8 - Projeto Cadastro - Integração com Banco de Dados(Firestore): CSS Modularizado

---
+++ Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,10 @@
+{/* Aula 8 - Projeto Cadastro - Integração com Banco de Dados(Firestore): CSS Modularizado */}
+
+import styles from '../styles/Estiloso.module.css'
+export default function Estiloso(){
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    )
+}
