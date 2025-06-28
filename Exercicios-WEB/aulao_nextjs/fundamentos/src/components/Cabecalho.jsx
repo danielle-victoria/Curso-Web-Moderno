4f48f5d0829cf7148b67156dd99318dbf98e8798
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-06-28 17:45:10 -0300

    Aula 9 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Usando Componente

---
+++ Exercicios-WEB/aulao_nextjs/fundamentos/src/components/Cabecalho.jsx
@@ -0,0 +1,9 @@
+{/* Aula 9 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Usando Componente */}
+
+export default function Cabecalho(){
+    return (
+        <header>
+            <h1>Fundamentos de Next.js & React</h1>
+        </header>
+    )
+}
---
+++ Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,18 @@
+import Cabecalho from "../components/Cabecalho";
+
+{/* Aula 9 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Usando Componente */}
+
+export default function Exemplo(){
+    return (
+        //<Cabecalho />
+
+        /*Para elementos repetidos <div> ou <> (mais indicado)*/
+
+        <>
+            <Cabecalho/>
+            <Cabecalho/>
+
+        </>
+    )
+}
+
