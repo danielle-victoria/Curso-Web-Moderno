386e416b7bd84e70d378233bc0f661a87befa0fa
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-07-01 17:44:24 -0300

    Aula 11 - Projeto Cadastro - Integração com Banco de Dados(Firestore):  Navegação entre Componentes

--- Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,9 +1,13 @@
 {/* Aula 8 - Projeto Cadastro - Integração com Banco de Dados(Firestore): CSS Modularizado */}
+{/* Aula 11 - Projeto Cadastro - Integração com Banco de Dados(Firestore):  Navegação entre Componentes */}
 
 import styles from '../styles/Estiloso.module.css'
+import Link from 'next/link'
+
 export default function Estiloso(){
     return (
         <div className={styles.roxo}>
+            <Link href="/">Voltar</Link>
             <h1>Estilo usando CSS Módulos</h1>
         </div>
     )
--- Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/index.jsx
+++ Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -1,8 +1,10 @@
 {/* Aula 4 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Primeiro Componente & Rotas */}
 {/* Aula 5 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Entendendo o JSX */}
+{/* Aula 11 - Projeto Cadastro - Integração com Banco de Dados(Firestore):  Navegação entre Componentes */}
 
+import Link from 'next/link'
 
-export default function Inicio(){
+/*export default function Inicio(){
     //"<h1>Título</h1>" - Assim não é interpretado como html
     return(
         <div>
@@ -11,4 +13,15 @@ export default function Inicio(){
         </div>
         
     ) 
+}*/
+
+
+export default function Inicio() {
+    return (
+        <div>
+            <Link href="/estiloso">
+                Estiloso
+            </Link>
+        </div>
+    )
 }
