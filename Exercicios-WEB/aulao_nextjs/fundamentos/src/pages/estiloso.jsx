d072e9649ecfb5d137fd03040a8af0ddfbfdd766
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-07-03 16:49:05 -0300

    Aula 12 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Componente Layout

--- Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ Exercicios-WEB/aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,14 +1,17 @@
 {/* Aula 8 - Projeto Cadastro - Integração com Banco de Dados(Firestore): CSS Modularizado */}
 {/* Aula 11 - Projeto Cadastro - Integração com Banco de Dados(Firestore):  Navegação entre Componentes */}
 
+import Layout from '../components/Layout'
 import styles from '../styles/Estiloso.module.css'
 import Link from 'next/link'
 
 export default function Estiloso(){
     return (
-        <div className={styles.roxo}>
-            <Link href="/">Voltar</Link>
-            <h1>Estilo usando CSS Módulos</h1>
+        <Layout titulo="Exemplo de CSS Modularizado">
+            <div className={styles.roxo}>
+                <h1>Estilo usando CSS Módulos</h1>
         </div>
+        </Layout>
+        
     )
 }
