c3e1eccffeb97d0808ba8e0bcf28cd035d73d15d
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-07-05 15:57:50 -0300

    Aula 21 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Modelos de Pré-Renderização

--- Exercicios-WEB/aulao_nextjs/fundamentos/src/components/Navegador.jsx
+++ Exercicios-WEB/aulao_nextjs/fundamentos/src/components/Navegador.jsx
@@ -1,11 +1,12 @@
 {/* Aula 13 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Componente Navegador */}
+{/* Aula 21 - Projeto Cadastro - Integração com Banco de Dados(Firestore): Modelos de Pré-Renderização */}
 
 import Link from "next/link"
 import styles from '../styles/Navegador.module.css'
 
 export default function Navegador(props){
     return (
-        <Link href={props.destino}>
+        <Link href={props.destino} passHref>
             <div className={styles.navegador} style={{
                 backgroundColor: props.cor ?? 'dodgerblue'
             }}>
