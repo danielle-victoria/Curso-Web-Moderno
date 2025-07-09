f0eb101d57ac522f3049010f8bfbe2d6a86bf98d
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2025-07-09 15:38:52 -0300

    Aula 36 - Projeto Cadastro - Integração com Banco de Dados(Firestore):  Configurando Firebase no Projeto

---
+++ Exercicios-WEB/projeto-cadastro-next/next-crud/src/backend/config.ts
@@ -0,0 +1,15 @@
+{/* Aula 36 - Projeto Cadastro - Integração com Banco de Dados(Firestore):  Configurando Firebase no Projeto */}
+
+import firebase from "firebase"
+import 'firebase/firestore'
+
+if(!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectid: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    })
+}
+
+
+export default firebase
