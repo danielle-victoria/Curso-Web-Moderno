cc17ddcead93dbfbaca11929bd183e367236aa96
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2026-03-02 17:49:23 -0300

    Aula 7 - Projeto Calculadora (Vue): Implementação da Lógica

--- Exercicios-WEB/vue/calculator/src/main/CalculatorComponent.vue
+++ Exercicios-WEB/vue/calculator/src/main/CalculatorComponent.vue
@@ -2,10 +2,11 @@
 <!-- Aula 4 - Projeto Calculadora (Vue): Componente Button #01 -->
 <!-- Aula 5 - Projeto Calculadora (Vue): Componente Display -->
 <!-- Aula 6 - Projeto Calculadora (Vue): Componente Button #02 -->
+<!-- Aula 7 - Projeto Calculadora (Vue): Implementação da Lógica -->
 
 <template>
     <div class="calculator">
-        <DisplayComponent value="1000"/>
+        <DisplayComponent :value="displayValue"/>
         <ButtonComponent label="AC" triple @onClick="clearMemory"/>
         <ButtonComponent label="/" operation @onClick="setOperation"/>
         <ButtonComponent label="7"  @onClick="addDigit"/>
@@ -33,16 +34,78 @@ import ButtonComponent from '../components/ButtonComponent'
 
 
 export default {
+    data: function() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+             operation: null,
+             values: [0, 0],
+             current: 0
+        }
+    },
     components: { ButtonComponent, DisplayComponent},
     methods: {
         clearMemory() {
             //console.log('Limpar Memória!')
+
+            Object.assign(this.$data, this.$options.data())
         },
         setOperation(operation) {
             //console.log('Operação' + operation)
+
+            if(this.current === 0) {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === "="
+                const currentOperation = this.operation
+
+                //const values = [...this.values]
+                try {
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`)
+                } catch(e) {
+                    //values[0] = this.values[0]
+                    this.$emit('onError', e)
+                }
+
+               this.values[1] = 0
+
+                //this.displayValue = values[0].toString()
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+                //this.values = values
+            }
         },
         addDigit(n) {
             //console.log('Dígito ' + n)
+
+            if(n === "." && this.displayValue.includes('.')) {
+                return
+            }
+
+            const clearDisplay = this.displayValue === "0"
+                || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+            
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            //Alternativa 1
+
+            this.values[this.current] = displayValue
+
+            // Alternativa 2
+            /*if(n !== ".") {
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue
+                //console.log(this.values)
+            }*/
         }
     }
 }
