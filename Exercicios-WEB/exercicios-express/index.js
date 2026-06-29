bec037b24f40315750edeba237a34b7f0260c505
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2026-06-29 19:13:00 -0300

    Aula 5 - Express: Tratando a Primeira Requisição

--- Exercicios-WEB/exercicios-express/index.js
+++ Exercicios-WEB/exercicios-express/index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+/*app.use */
+app.all ('/opa', (req, res) => {
+    res.send('Estou <b>bem</b>!')
+})
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
